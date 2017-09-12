a87e475992d691d2cc2a6a104626465a59fec388
Robert Hurst <rhurst@e-xact.com>
2017-09-11 18:06:54 -0700

    implement cli options

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,41 @@
 extern crate tritium;
-
-use tritium::Proxy;
+#[macro_use]
+extern crate clap;
+use std::process;
+use std::path::PathBuf;
+use clap::{App, Arg, ArgMatches};
 
 fn main() {
-    let mut proxy = Proxy::new();
-    // TODO: We need to discuss what our configuration looks like, and how to load it.
-    // proxy.load_config();
+    let matches = get_cli_matches();
+    let config_path = get_config_path(matches);
+
+    let mut proxy = tritium::Proxy::new();
+    proxy.load_config(config_path);
     proxy.run();
+}
+
+fn get_cli_matches<'a>() -> ArgMatches<'a> {
+    App::new(crate_name!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .version(crate_version!())
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("CONFIG_FILE")
+                .help("Sets the config location")
+                .takes_value(true),
+        )
+        .get_matches()
+}
+
+fn get_config_path(matches: ArgMatches) -> PathBuf {
+    match matches.value_of("config") {
+        Some(p) => PathBuf::from(p),
+        None => {
+            println!("CONFIG_FILE is required. See -h for help.");
+            process::exit(1);
+        }
+    }
 }
--- src/proxy.rs
+++ src/proxy.rs
@@ -1,10 +1,30 @@
+use std::path::PathBuf;
 
-pub struct Proxy {}
+pub struct Proxy {
+    config_path: Option<PathBuf>,
+}
 
 impl Proxy {
     pub fn new() -> Self {
-        Self {}
+        Self { config_path: None }
     }
 
-    pub fn run(&mut self) {}
+    pub fn load_config<P>(&mut self, config_path: P)
+    where
+        P: Into<PathBuf>,
+    {
+        self.config_path = Some(config_path.into());
+        // TODO: here we will want to call instance methods on Proxy in order to configure the
+        // instance.
+    }
+
+    pub fn config_path(&self) -> Option<&PathBuf> {
+        self.config_path.as_ref()
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            // Where the magic happens
+        }
+    }
 }
